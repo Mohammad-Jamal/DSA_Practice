09d5981523abe3f7d400631c712b440795d75f9d
Mohammad-Jamal <m.mohammadjamal1961@gmail.com>
2024-03-19 15:12:28 +0530

    Practised till now

--- Day20.cpp
+++ Day20.cpp
@@ -44,4 +44,64 @@ int main()
   }
 
   cout << first << " " << last;
+
+  // todo Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+  int arr[5] = {1, 2, 3, 5, 6};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int start = 0, end = n - 1, mid, ans = n;
+  int target;
+  cout << "Enter the target :";
+  cin >> target;
+
+  while (start <= end)
+  {
+    mid = start + (end - start) / 2;
+
+    if (arr[mid] == target)
+    {
+      ans = mid;
+      break;
+    }
+    else if (arr[mid] < target)
+      start = mid + 1;
+    else
+    {
+      ans = mid;
+      end = mid - 1;
+    }
+  }
+  cout << "Index of target " << target << " is " << ans;
+
+  // todo Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+  // todo You must not use any built-in exponent function or operator.
+  int x;
+  cout << "Enter the number :";
+  cin >> x;
+  int start = 0, end = x, mid, ans;
+
+  // if (x < 2)
+  // cout<<x;
+  // return x;
+
+  while (start <= end)
+  {
+    mid = start + (end - start) / 2;
+
+    if (mid * mid == x)
+    {
+      ans = mid;
+      break;
+    }
+    else if (mid * mid < x)
+    {
+      ans = mid;
+      start = mid + 1;
+    }
+    else
+      end = mid - 1;
+  }
+
+  cout << ans;
 }
