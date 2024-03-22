224192e5f38ab54c6cf47ef2007e34368074ac2f
Mohammad-Jamal <m.mohammadjamal1961@gmail.com>
2024-03-22 22:39:50 +0530

    Practised till now

--- Day21.cpp
+++ Day21.cpp
@@ -4,27 +4,84 @@ using namespace std;
 int main()
 {
   // todo 852. Peak Index in a Mountain Array
-  int arr[8] = {2, 4, 6, 10, 8, 5, 3, 1};
+  // int arr[8] = {2, 4, 6, 10, 8, 5, 3, 1};
+  // int n = sizeof(arr) / sizeof(arr[0]);
+  // int start = 0, end = n - 1, mid;
+
+  // while (start <= end)
+  // {
+  //   mid = end + (start - end) / 2;
+
+  //   // peak element index
+  //   if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+  //   {
+  //     cout << mid;
+  //     break;
+  //   }
+  //   // move right side
+  //   else if (arr[mid] > arr[mid - 1])
+  //     start = mid + 1;
+  //   // move left side
+  //   else
+  //     end = mid - 1;
+  // }
+
+  // return -1; //? if does not exist
+
+  // todo 153. Find Minimum in Rotated Sorted Array
+
+  // int arr[6] = {2, 4, 5, 6, 1, 3};
+  // int n = sizeof(arr) / sizeof(arr[0]);
+  // int start = 0, end = n - 1, mid, ans = arr[0];
+
+  // while (start <= end)
+  // {
+  //   mid = start + (end - start) / 2;
+
+  //   //? left side sorted
+  //   if (arr[mid] >= arr[0])
+  //     start = mid + 1;
+  //   //? right side sorted
+  //   else //?  means arr[mid] < arr[0]
+  //   {
+  //     ans = arr[mid];
+  //     end = mid - 1;
+  //   }
+  // }
+
+  // cout << "Minimum number is " << ans;
+
+  // todo 33. Search in Rotated Sorted Array
+  int arr[6] = {4, 5, 6, 1, 2, 3};
   int n = sizeof(arr) / sizeof(arr[0]);
-  int start = 0, end = n - 1, mid;
+  int start = 0, end = n - 1, mid, ans = -1, target;
+  cout << "Enter the target :";
+  cin >> target;
 
   while (start <= end)
   {
-    mid = end + (start - end) / 2;
+    mid = start + (end - start) / 2;
 
-    // peak element index
-    if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1])
+    if (arr[mid] == target)
     {
-      cout << mid;
+      ans = mid;
       break;
     }
-    // move right side
-    else if (arr[mid] > arr[mid - 1])
-      start = mid + 1;
-    // move left side
+    else if (arr[mid] >= arr[0])
+    {
+      if (arr[start] <= target && arr[mid] >= target)
+        end = mid - 1;
+      else
+        start = mid + 1;
+    }
     else
-      end = mid - 1;
+    {
+      if (arr[mid] <= target && arr[end] >= target)
+        start = mid + 1;
+      else
+        end = mid - 1;
+    }
   }
 
-  return -1; //? if does not exist
+  cout << ans;
 }
