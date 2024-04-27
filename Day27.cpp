241319a06edfdb0580b9a87bf049c02a741ddb8c
Mohammad-Jamal <m.mohammadjamal1961@gmail.com>
2024-04-27 22:12:26 +0530

    Practised till now

--- Day27.cpp
+++ Day27.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-  //todo 42.Trapping Rain Water leet_code - hard
+  // //todo 42.Trapping Rain Water leet_code - hard
   vector<int> height = {4, 2, 0, 5, 2, 6, 2, 3};
   int n = height.size(), water = 0;
   int left_max = 0, right_max = 0, max_height = height[0], index = 0;
@@ -36,4 +36,50 @@ int main()
       right_max = height[i];
   }
   cout << "Total units of water : " << water;
+
+  // todo Triplet sum in Array  - GFG
+
+  int arr[6] = {1, 4, 45, 8, 10, 6};
+  int n = sizeof(arr) / sizeof(arr[0]);
+  int X = 24; //? target
+
+  //* Sort the array first
+  //! I am using bubble sort
+
+  for (int i = n - 2; i >= 0; i--)
+  {
+    for (int j = 0; j <= i; j++)
+    {
+      if (arr[j] > arr[j + 1])
+        swap(arr[j], arr[j + 1]);
+    }
+  }
+
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+
+  cout << "Array is sorted";
+
+  //! now solve the problem
+
+  int is_triplet_sum = 0;
+  cout << "nx";
+  for (int i = 0; i < n - 2; i++)
+  {
+    int ans = X - arr[i];
+    int start = i + 1, end = n - 1;
+    while (start < end)
+    {
+      if (arr[start] + arr[end] == ans)
+        is_triplet_sum = 1;
+      else if (arr[start] + arr[end] > ans)
+        end--;
+      else
+        start++;
+    }
+  }
+  if (is_triplet_sum)
+    cout << "Triplet sum is possible";
+  else
+    cout << "Triplet sum is not possible";
 }
